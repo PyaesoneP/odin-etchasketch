0160a6dd82892ddb32a6bd4b1f713931e6f1c118
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-01-04 09:42:43 +0630

    Add the progressively darker functionality on hover on grids

--- script/script.js
+++ script/script.js
@@ -31,11 +31,53 @@ function getGridNum (num) {
             let grid = document.createElement('div');
             grid.classList.add('grid');
             gridColumn.appendChild(grid);
+            grid.style.filter = 'brightness(100%)';
             grid.addEventListener('mouseover', () => {
                 grid.style.backgroundColor = generateRandomColor();
-            })
-            grid.addEventListener('mouseover', () => {
-                grid.style.filter = 'brightness(70%);';
+                switch (grid.style.filter) {
+                    case 'brightness(100%)':
+                        grid.style.filter = 'brightness(90%)';
+                        break;
+            
+                    case 'brightness(90%)':
+                        grid.style.filter = 'brightness(80%)';
+                        break;
+            
+                    case 'brightness(80%)':
+                        grid.style.filter = 'brightness(70%)';
+                        break;
+            
+                    case 'brightness(70%)':
+                        grid.style.filter = 'brightness(60%)';
+                        break;
+            
+                    case 'brightness(60%)':
+                        grid.style.filter = 'brightness(50%)';
+                        break;
+
+                    case 'brightness(50%)':
+                        grid.style.filter = 'brightness(40%)';
+                        break;
+
+                    case 'brightness(40%)':
+                        grid.style.filter = 'brightness(30%)';
+                        break;
+
+                    case 'brightness(30%)':
+                        grid.style.filter = 'brightness(20%)';
+                        break;
+
+                    case 'brightness(20%)':
+                        grid.style.filter = 'brightness(10%)';
+                        break;
+
+                    case 'brightness(10%)':
+                        grid.style.filter = 'brightness(0%)';
+                        break;
+            
+                    default:
+                        break;
+                }
             })
         }
     }
